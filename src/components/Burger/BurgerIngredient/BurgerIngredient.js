c89d67773a3d719c8198480d2ab1ce99fe972b5d
valeriy mixailov <ethno4ka@gmail.com>
2020-04-11 17:36:09 +0300

    Designing the Burger Component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './burgerIngredient.css';
+import './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
   render () {
