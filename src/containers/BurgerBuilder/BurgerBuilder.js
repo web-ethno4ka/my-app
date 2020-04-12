7e0251b06760eff6faa85fa53111f9e4978644b6
valeriy mixailov <ethno4ka@gmail.com>
2020-04-12 20:23:32 +0300

    Styling of BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   state = {
@@ -8,14 +9,14 @@ class BurgerBuilder extends Component {
       salad: 1,
       bacon: 2,
       cheese: 1,
-      meat: 1
+      meat: 2
     }
   }
   render () {
     return (
       <Auxiliary>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build controls</div>
+        <BuildControls />
       </Auxiliary>
     );
   }
